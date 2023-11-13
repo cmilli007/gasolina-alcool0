0a57c6c46680874f9fc40c28d467c67d36a087b6
camili <camili.costa07@gmail.com>
2023-11-13 12:55:58 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿decimal alcool  = Convert.ToDecimal(Console.ReadLine());
+decimal gasolina  = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine ("Alcool ou Gasolina");
+Console.WriteLine ("Digite o valor do alcool:(R$)");
+Console.WriteLine ("Digite o valor da gasolina:(R$)");
+
+string recomendacao;
+if (AbastecerComGasolina(alcool, gasolina))
+{
+    recomendacao = "Gasolina";
+}
+else
+{
+    recomendacao = "Alcool";
+}
+double razao = RazaoAlcoolGasolina(alcool, gasolina) * 100;
+
+Console.WriteLine($"O preço do Alcool corresponde a {razao:N1}% do preço da gasolina.");
+Console.WriteLine($"Recomendação: Abasteça com {recomendacao.ToUpper()}.");
+
+double RazaoAlcoolGasolina(decimal precoAlcool, decimal precoGasolina)
+{
+    return Convert.ToDouble(precoAlcool / precoGasolina);
+}
+
+bool AbastecerComGasolina(decimal precoAlcool, decimal precoGasolina)
+{
+    const double MinRazaoUsoGasolina = 0.73;
+    return RazaoAlcoolGasolina(precoAlcool, precoGasolina) >= MinRazaoUsoGasolina;
+}
